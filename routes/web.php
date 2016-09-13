2593f60d5f681588a8b28f0be5eec6921f58ab23
Ryan Kennedy <ryan-tn-fc@hotmail.co.uk>
2016-09-13 17:52:51 +0100

    fixing login page

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('dashboard', function () {
 	if (Auth::check(Auth::user())) {
 		return view('dashboard');
 	} else {
-		return view('login', ['fail' => 'You must be logged in to view this page']);
+		return view('auth.login', ['fail' => 'You must be logged in to view this page']);
 	}
 
     
@@ -27,29 +27,20 @@ Route::get('dashboard', function () {
 
 Route::get('logout', function() {
     Auth::logout();
-    return view('login');
+    return view('auth.login');
 });
 
 
 Auth::routes();
 
 
-Route::get('/home', 'HomeController@index');
+Route::get('/', 'HomeController@index');
 
 
 Route::group(['middleware' => 'auth'], function(){
-	// Route::get('/events/new', 'EventsController@create');
-	// Route::post('/events/new', 'EventsController@newEvent');
-	// Route::get('/events/{id}/edit', ['as' => 'event_edit', 'uses' => 'EventsController@edit']);
-	// Route::post('/events/{id}/edit', 'EventsController@update');
-	// Route::get('/events', 'EventsController@show');
-	// Route::get('events/{id}', ['as' => 'event_single', 'uses' => 'EventsController@singleEvent']);
 	 Route::post('events/{id}/attachment', ['as' => 'addAttachment', 'uses' => 'EventsController@addAttachment']);
 	 Route::post('events/{id}/attachment/{aid}', ['as' => 'deleteAttachment', 'uses' => 'EventsController@deleteAttachment']);
-	// Route::get('events/{id}/delete', "EventsController@delete");
-
 	Route::resource('events', 'EventsController');
-
 	Route::get('users/{id}', 'UsersController@show');
 });
 
